c7c6fb24e14ffb9f27815f816abb1dbb9e83c670
edrummon <edrummon@mail.umw.edu>
2010-02-20 17:00:41 -0700

    added band search option

--- searchbar.php
+++ searchbar.php
@@ -25,3 +25,17 @@
 		</form>
 	</td></tr>
 	</table>
+
+
+        <table border="1" bordercolor="white" cellpadding="5" cellspacing="0" width="100%">
+	<tr bgcolor="<?php echo($cellbg); ?>"><td align="center">
+		<font size="5" color="<?php echo($headingtext); ?>"><b>Venue Search</b></font>
+	</td></tr>
+	<tr bgcolor="white"><td align="center">
+		<font size="2" color="black"><b>Enter a band name to search:</b></font>
+		<form action="artistSearch.php" method="post">
+		<input type="text" name="searchbox">
+                <input name="submit" type="submit" value="Search">
+		</form>
+	</td></tr>
+	</table>
