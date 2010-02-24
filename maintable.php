903eb4d2c769aaf59e8b40708f03d3166ef6541b
vroycroft <vroyc1kf@mail.umw.edu>
2010-02-23 21:28:14 -0700

    randomly selects a featured band and displays band name, about, and picture

--- maintable.php
+++ maintable.php
@@ -3,12 +3,20 @@
 		<font size="5" color="<?php echo($headingtext); ?>"><b>Featured Band</b></font>
 	</td></tr>
 	<tr bgcolor="white"><td align="center">
-		<font size="3" color="black"><b>Text goes here</b></font>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<p>&nbsp;</p>
-		<br />
+         <?php
+	  $query = "SELECT * FROM $table ORDER BY RAND() LIMIT 1";
+	  $result = mysqli_query($db, $query)
+   		or die("Error Querying Database");
+	  $row = mysqli_fetch_array($result);
+	  $name = $row['name'];
+	  $about = $row['about'];
+	  $image = $row['image'];
+
+	  echo "$name";
+	  echo "<br/><br/> $about";
+	  echo "<br/><br/> <img src =\"$image\" style = \"width: 350px; height: 275 px;\"/>\n";
+           mysqli_close($db);
+         ?>
+	<br />
 	</td></tr>
 	</table>
