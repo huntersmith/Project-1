663ebfab6f08946fab79f1ffcddec86f3390f656
vroycroft <vroyc1kf@mail.umw.edu>
2010-03-01 18:56:59 -0500

    selects a random venue and displays basic info and picture

--- maintable.php
+++ maintable.php
@@ -16,6 +16,26 @@
 	  echo "$name";
 	  echo "<br/><br/> $about";
 	  echo "<br/><br/> <img src =\"$image\" style = \"width: 350px; height: 275 px;\"/>\n";
+           
+         ?>
+	<br />
+	</td></tr>
+	<tr bgcolor="<?php echo($cellbg); ?>"><td align="center">
+		<font size="5" color="<?php echo($headingtext); ?>"><b>Featured Venue</b></font>
+	</td></tr>
+	<tr bgcolor="white"><td align="center">
+         <?php
+	  $query = "SELECT * FROM venues ORDER BY RAND() LIMIT 1";
+	  $result = mysqli_query($db, $query)
+   		or die("Error 1 Querying Database");
+	  $row = mysqli_fetch_array($result);
+	  $name = $row['name'];
+	  $about = $row['about'];
+	  $image = $row['image'];
+
+	  echo "$name";
+	  echo "<br/><br/> $about";
+	  echo "<br/><br/> <img src =\"$image\" style = \"width: 350px; height: 275 px;\"/>\n";
            mysqli_close($db);
          ?>
 	<br />
