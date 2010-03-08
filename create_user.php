0643389ba4d2bb0c5040b4ac201ce96763855f4d
Hunter Smith <huntersmi@gmail.com>
2010-03-08 15:30:10 -0500

    Created an entry form for regular users

---
+++ create_user.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Login Information</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+	<div id="contents">
+  		<h1>Create Your Login</h1>
+
+    		<label for="street_address">First Name:</label>
+    		<input type="text" id="first_name" name="first_name"/><br/>
+
+    		<label for="street_address">Last  Name:</label>
+    		<input type="text" id="last_name" name="last_name"/><br/>
+
+    		<label for="user_name">Login Name:</label>
+    		<input type="text" id="user_name" name="name"/><br/>
+
+    		<label for="street_address">Email Address:</label>
+    		<input type="text" id="email" name="email"/><br/>
+    
+    		<label for="city">Password:</label>
+    		<input type="text" id="password" name="password"/><br/>
+
+    		<input type="submit" value="Create User" name="submit"/>
+	</div>
+</body>
+
+</html>
