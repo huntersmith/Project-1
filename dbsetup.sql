46a9427a8a4bc2314e3edb8eac28f975ba8d1fba
lbain <lbain@mail.umw.edu>
2010-03-13 16:41:35 -0700

    added email field to the login table

--- dbsetup.sql
+++ dbsetup.sql
@@ -101,5 +101,6 @@ CREATE TABLE IF NOT EXISTS `login` (
   `password` varchar(100) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
+  `email` varchar(60) NOT NULL,
   PRIMARY KEY (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
