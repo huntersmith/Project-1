6653e1f2ab5db95e760cb3c15408198cece4d937
Hunter Smith <huntersmi@gmail.com>
2010-03-21 17:29:57 -0400

    Trying to figure out how to get about.php to display in the index.php

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+echo "<br><P ALIGN = 'left'><font size = '5'><b><font color = 'blue'>About Page for Band Site CPSC 350: </b></font></font></P>";
+echo "<ul>";
+echo "<li>Scrum Leader: Lucy Bain</li>";
+echo "<li>Josh Abbot</li>";
+echo "<li>Ned Drummond</li>";
+echo "<li>Vanessa Roycroft</li>";
+echo "<li>Hunter Smith</li>";
+echo "<li>Kaylee Wichert</li>";
+echo "</ul>";
+?>
