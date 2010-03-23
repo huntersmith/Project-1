de7905028b4c61a607761cc7e261c5fed62f9595
Hunter Smith <huntersmi@gmail.com>
2010-03-23 15:37:33 -0400

    Made the about.php appear in the index.php format.

--- about.php
+++ about.php
@@ -1,3 +1,5 @@
+<table border="1" bordercolor="white" cellpadding="0" cellspacing="0" width="100%">
+<tr bgcolor="white"><td align="center">
 <?php
 echo "<br><P ALIGN = 'left'><font size = '5'><b><font color = 'blue'>About Page for Band Site CPSC 350: </b></font></font></P>";
 echo "<ul>";
@@ -7,5 +9,6 @@ echo "<li>Ned Drummond</li>";
 echo "<li>Vanessa Roycroft</li>";
 echo "<li>Hunter Smith</li>";
 echo "<li>Kaylee Wichert</li>";
+echo "<font size = '5'><b><font color = 'blue'>More Coming soon!</b></font></font>";
 echo "</ul>";
 ?>
--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php
-echo "<a href='install.php'>Source the Database Files</a>";
-echo "<a href='about.php'>About our Site</a>";
+echo "<a href='install.php'>Source the Database Files</a></br>";
+echo "<a href='index.php?page=about.php'>About our Site</a>";
 ?>
