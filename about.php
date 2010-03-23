79a5bf70d134a3fac30653c13db65179cfd4617c
Hunter Smith <huntersmi@gmail.com>
2010-03-23 15:48:25 -0400

    Modified about.php to look better.

--- about.php
+++ about.php
@@ -1,14 +1,15 @@
 <table border="1" bordercolor="white" cellpadding="0" cellspacing="0" width="100%">
-<tr bgcolor="white"><td align="center">
+<tr bgcolor="white"><td align="left">
 <?php
-echo "<br><P ALIGN = 'left'><font size = '5'><b><font color = 'blue'>About Page for Band Site CPSC 350: </b></font></font></P>";
-echo "<ul>";
+echo "<P ALIGN = 'left'><font size = '5'><b><font color = 'blue'>About Page for Band Site CPSC 350: </b></font></font></P>";
 echo "<li>Scrum Leader: Lucy Bain</li>";
 echo "<li>Josh Abbot</li>";
 echo "<li>Ned Drummond</li>";
 echo "<li>Vanessa Roycroft</li>";
 echo "<li>Hunter Smith</li>";
 echo "<li>Kaylee Wichert</li>";
+echo "</br>";
 echo "<font size = '5'><b><font color = 'blue'>More Coming soon!</b></font></font>";
-echo "</ul>";
 ?>
+</td></tr>
+</table>
