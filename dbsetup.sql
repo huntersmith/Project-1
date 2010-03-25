ffe163e13ce4afbd3e405c18eca67a780eff2441
Hunter Smith <huntersmi@gmail.com>
2010-03-24 22:04:01 -0400

    Changed the images in dbsetup.sql

--- dbsetup.sql
+++ dbsetup.sql
@@ -28,16 +28,16 @@ CREATE TABLE IF NOT EXISTS `bands`.`bandinfo` (
 -- Dumping data for table `bandinfo`
 --
 
-INSERT INTO `bands`.`bandinfo` (`band_id`, `name`, `street_address`, `city`, `state`, `genre`, `about`, `members`) VALUES
-(NULL, 'Test Band', '1200 Anywhere St.', 'Fredericksburg', 'VA', 'Country, Blue Grass', NULL, 'Bob, Jane, Sue, Alex'),
-(NULL, 'Another Test', '1302 Street Ln', 'Alexandria', 'VA', 'Pop, Rock', NULL, 'Jeff, Julie, Jordan'),
-(NULL, 'The Beatles', '465 Rosebud Ave', 'Gregory', 'SD', 'Rock and Roll', NULL, 'John Lennon, Paul McCartney, George Harrison, Ringo Star'),
-(NULL, 'Blue', '333 SW 1st St', 'Afton', 'OK', 'Techno', NULL, 'Linda, Jessica, Dan'),
-(NULL, 'Sunshine', '270 Charles St', 'Fredericksburg', 'VA', 'Pop', NULL, 'Elizabeth, Ben, Zac'),
-(NULL, 'The Judges','200 UCLA Medical Plaza', 'Los Angeles', 'CA', 'Heavy metal', NULL, 'Jack Sonni, Ken Lopez, Howard Schilling, Charles Andrews'),
-(NULL, 'Led Zeppelin','501 University Avenue','Fort Collins', 'CO', 'Rock', NULL, 'Jimmy Page, John Paul Jones, Robert Plant, John Bonham'),
-(NULL, 'Pink Floyd', '147 Shenango Avenue', 'Sharon', 'PA', 'Rock', NULL, 'Syd Barrett, David Gilmour, Bob Klose, Nick Mason, Roger Waters, Richard Wright'),
-(NULL, 'The Beach Boys','75 3rd Avenue', 'New York', 'NY', 'Sunshine Pop', NULL, 'Al Jardine, Bruce Johnston, Mike Love, Brian Wilson');
+INSERT INTO `bands`.`bandinfo` (`band_id`, `name`, `street_address`, `city`, `state`, `image`, `genre`, `about`, `members`) VALUES
+(NULL, 'Test Band', '1200 Anywhere St.', 'Fredericksburg', 'VA', 'default.jpeg', 'Country, Blue Grass', NULL, 'Bob, Jane, Sue, Alex'),
+(NULL, 'Another Test', '1302 Street Ln', 'Alexandria', 'VA', 'default.jpeg', 'Pop, Rock', NULL, 'Jeff, Julie, Jordan'),
+(NULL, 'The Beatles', '465 Rosebud Ave', 'Gregory', 'SD', 'beatles_abbey-road.jpeg', 'Rock and Roll', NULL, 'John Lennon, Paul McCartney, George Harrison, Ringo Star'),
+(NULL, 'Blue', '333 SW 1st St', 'Afton', 'OK', 'default.jpeg', 'Techno', NULL, 'Linda, Jessica, Dan'),
+(NULL, 'Sunshine', '270 Charles St', 'Fredericksburg', 'VA', 'default.jpeg', 'Pop', NULL, 'Elizabeth, Ben, Zac'),
+(NULL, 'The Judges','200 UCLA Medical Plaza', 'Los Angeles', 'CA', 'default.jpeg', 'Heavy metal', NULL, 'Jack Sonni, Ken Lopez, Howard Schilling, Charles Andrews'),
+(NULL, 'Led Zeppelin','501 University Avenue','Fort Collins', 'CO', 'default.jpeg', 'Rock', NULL, 'Jimmy Page, John Paul Jones, Robert Plant, John Bonham'),
+(NULL, 'Pink Floyd', '147 Shenango Avenue', 'Sharon', 'PA', 'default.jpeg', 'Rock', NULL, 'Syd Barrett, David Gilmour, Bob Klose, Nick Mason, Roger Waters, Richard Wright'),
+(NULL, 'The Beach Boys','75 3rd Avenue', 'New York', 'NY', 'default.jpeg', 'Sunshine Pop', NULL, 'Al Jardine, Bruce Johnston, Mike Love, Brian Wilson');
 
 
 -- --------------------------------------------------------
