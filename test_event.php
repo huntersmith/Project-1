e014935ff2c04a81e56c7e2cae3ad43adb914153
lbain <lbain@mail.umw.edu>
2010-03-24 18:12:39 -0700

    stoped anything with band2 and band3 from showing to keep tables in 3NF

--- test_event.php
+++ test_event.php
@@ -15,8 +15,8 @@
   $time = mysqli_real_escape_string($db, trim($_POST['time']));
   $venue = mysqli_real_escape_string($db, trim($_POST['venue']));
   $band1 = mysqli_real_escape_string($db, trim($_POST['band1']));
-  $band2 = mysqli_real_escape_string($db, trim($_POST['band2']));
-  $band3 = mysqli_real_escape_string($db, trim($_POST['band3']));
+/*  $band2 = mysqli_real_escape_string($db, trim($_POST['band2']));
+  $band3 = mysqli_real_escape_string($db, trim($_POST['band3']));*/
 
 $query = "SELECT venue_id FROM venues WHERE name = '$venue'";
 
@@ -34,7 +34,7 @@ $result = mysqli_query($db, $query)
 
 $row = mysqli_fetch_array($result);
 $band_id1 = $row['band_id'];
-
+/*
 $query = "SELECT band_id FROM $table WHERE name = '$band2'";
 
 $result = mysqli_query($db, $query)
@@ -52,6 +52,8 @@ $row = mysqli_fetch_array($result);
 $band_id3 = $row['band_id'];
 
   $query = "INSERT INTO events (name, venue_id, date, time, band_id1, band_id2, band_id3) VALUES ('$name', '$venue_id', '$date', '$time', '$band_id1', '$band_id2', '$band_id3')";
+*/
+  $query = "INSERT INTO events (name, venue_id, date, time, band_id1, band_id2, band_id3) VALUES ('$name', '$venue_id', '$date', '$time', '$band_id1')";
   
 
   $result = mysqli_query($db, $query)
@@ -70,8 +72,8 @@ $query = "SELECT * FROM events WHERE name = '$name'";
 	$date = $row['date'];
   	$time = $row['time'];
   	$band_id1 = $row['band_id1'];
- 	$band_id2 = $row['band_id2'];
-	$band_id3 = $row['band_id3'];
+ 	/*$band_id2 = $row['band_id2'];
+	$band_id3 = $row['band_id3'];*/
   }
 
 $query = "SELECT name FROM venues WHERE venue_id = '$venue_id'";
@@ -85,7 +87,7 @@ $result = mysqli_query($db, $query)
     or die("Error 6 Querying Database");
 $row = mysqli_fetch_array($result);
 $band_name1 = $row['name'];
-
+/*
 $query = "SELECT name FROM bandinfo WHERE band_id = '$band_id2'";
 $result = mysqli_query($db, $query)
     or die("Error 6 Querying Database");
@@ -97,14 +99,14 @@ $result = mysqli_query($db, $query)
     or die("Error 6 Querying Database");
 $row = mysqli_fetch_array($result);
 $band_name3 = $row['name'];
-
+*/
   	echo "<h1>$name</h1>";
       echo "<p>$venue_name</p>";
       echo "<p>$date</p>";
       echo "<p>$time</p>";
       echo "<p>$band_name1</p>";
-	echo "<p>$band_name2</p>";
-	echo "<p>$band_name3</p>";
+/*	echo "<p>$band_name2</p>";
+	echo "<p>$band_name3</p>";*/
   
   mysqli_close($db);
 
