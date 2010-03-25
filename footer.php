63eb57930071e31976fa2d1166d4696d3dbca1a4
Hunter Smith <huntersmi@gmail.com>
2010-03-25 16:14:53 -0400

    Modified footer.php so it has the correct link.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <?php
-echo "<a href='index.php?page=install.bak.php'>Source the Database Files</a></br>";
+echo "<a href='index.php?page=install.php'>Source the Database Files</a></br>";
 echo "<a href='index.php?page=about.php'>About our Site</a>";
 ?>
