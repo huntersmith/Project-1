110a6e67e8d35cb39f55b1eb79cae2c437f90c02
unknown <Josh@.(none)>
2010-04-06 19:11:53 -0400

    Fixed spelling error

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <?php
 echo "<P ALIGN = 'left'><font size = '5'><b><font color = 'blue'>About Page for Band Site CPSC 350: </b></font></font></P>";
 echo "<li>Scrum Leader: Lucy Bain</li>";
-echo "<li>Josh Abbot</li>";
+echo "<li>Josh Abbott</li>";
 echo "<li>Ned Drummond</li>";
 echo "<li>Vanessa Roycroft</li>";
 echo "<li>Hunter Smith</li>";
